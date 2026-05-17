bd8a7f110bf6ee25b4eee976fb99e638523381fc
azad-727 <sazad3774@gmail.com>
2026-05-17 16:02:49 +0530

    chore: intial Spring Boot web setup

---
+++ rituals-api/src/main/java/com/rituals/api/RitualsApiApplication.java
@@ -0,0 +1,13 @@
+package com.rituals.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RitualsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RitualsApiApplication.class, args);
+	}
+
+}
